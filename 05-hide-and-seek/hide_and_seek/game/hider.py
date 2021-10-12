14dcbb7fc6d69377384c195121a63c24de45dbec
jaredee-byui <jaredee@byui.edu>
2021-10-12 17:49:56 -0600

    "completed hider class, keep ibuprofen nearby before coding.

--- 05-hide-and-seek/hide_and_seek/game/hider.py
+++ 05-hide-and-seek/hide_and_seek/game/hider.py
@@ -1,3 +1,24 @@
 import random
 
 # TODO: Define the Hider class here
+class Hider:
+
+    def __init__(self):
+        self.location = random.randint(1,1000)
+        self.distance = [0, 0]
+
+    def watch(self, location):
+        distance = abs(self.location - location)
+        self.distance.append(distance)
+
+    def get_hint(self):
+        hint = "(-.-) Maybe I'll take a nap. "
+        if self.distance[-1] == 0:
+            hint = "(0_0) you found me! "
+        elif self.distance[-1] > self.distance[-2]:
+            hint = "(^.^) Getting colder!"
+        elif self.distance[-1] < self.distance[-2]:
+            hint = "(>_<) Getting Warmer!"
+        return hint
+
+    
